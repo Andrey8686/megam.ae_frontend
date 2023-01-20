3d1e859dc8f6cbcbeed78b7cb4d8fa4206ba52e3
Andrey8686 <mr.starforce@gmail.com>
2023-01-20 18:52:00 +0300

    Save

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,21 +1,21 @@
-import MobMenu from "./modules/MobMenu.js";
-import Header from "./modules/Header.js";
-import NumberRunner from "./modules/NumberRunner.js";
-import ScrollActivator from "./modules/ScrollActivator.js";
-import Management from "./modules/Management.js";
-import ModalWindow from "./modules/ModalWindow.js";
-
-new Header()
-new MobMenu()
-new NumberRunner(".striving-block")
-new NumberRunner(".current-businesses")
-new Management()
-new ModalWindow()
-
-ScrollActivator('.founder', ['show'])
-ScrollActivator('.about-founder', ['show'])
-ScrollActivator('.current-businesses', ['number-runner-start', 'show'])
-ScrollActivator('.activity', ['show'])
+// import MobMenu from "./modules/MobMenu.js";
+// import Header from "./modules/Header.js";
+// import NumberRunner from "./modules/NumberRunner.js";
+// import ScrollActivator from "./modules/ScrollActivator.js";
+// import Management from "./modules/Management.js";
+// import ModalWindow from "./modules/ModalWindow.js";
+//
+// new Header()
+// new MobMenu()
+// new NumberRunner(".striving-block")
+// new NumberRunner(".current-businesses")
+// new Management()
+// new ModalWindow()
+//
+// ScrollActivator('.founder', ['show'])
+// ScrollActivator('.about-founder', ['show'])
+// ScrollActivator('.current-businesses', ['number-runner-start', 'show'])
+// ScrollActivator('.activity', ['show'])
 // ScrollActivator('.real-estate-block', ['show'])
 // ScrollActivator('.examples-fintech-projects', ['show'])
 
